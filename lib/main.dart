9ed4e7ceea22263d480d82f163cbf5dbd723af1f
unknown <ahm.ahm7785@gmail.com>
2021-01-13 11:10:22 +0500

    ejaz 1

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ejaz 1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         // This makes the visual density adapt to the platform that you run
